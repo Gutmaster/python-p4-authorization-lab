721bd77b099a92c944f8ce8da909da511a82360e
Chevy <vallchevy@gmail.com>
2024-08-14 11:46:47 -0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -18,6 +18,12 @@ db.init_app(app)
 
 api = Api(app)
 
+# @app.before_request
+# def check_if_logged_in():
+#     if not session['user_id'] \
+#         and request.endpoint == 'member_index' or request.endpoint == 'member_article':
+#         return {'error': 'Unauthorized'}, 401
+    
 class ClearSession(Resource):
 
     def delete(self):
@@ -87,12 +93,21 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        if not session['user_id']:
+            return {'error': 'Unauthorized'}, 401
+        
+        articles = [article.to_dict() for article in Article.query.filter_by(is_member_only=1).all()]
+        return make_response(articles, 200)
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        if not session['user_id']:
+            return {'error': 'Unauthorized'}, 401
+        
+        article = Article.query.filter(Article.id == id).first()
+
+        return article.to_dict(), 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
